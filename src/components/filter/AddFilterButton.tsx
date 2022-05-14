2527e42d36faa8b455d5e1a581faa22f84f3ca71
Arun Chithambaram <arun.chithambaram@newglobe.education>
2022-05-14 15:41:34 +0530

    Extracts API call to util

--- src/components/filter/AddFilterButton.tsx
+++ src/components/filter/AddFilterButton.tsx
@@ -3,48 +3,18 @@ import { Auth0ContextInterface } from '@auth0/auth0-react';
 
 import { ButtonDropdown, DropdownToggle, DropdownMenu } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ColumnItem from './ColumnItem';
+import ColumnList from './ColumnList';
 
-import { useRecoilState } from 'recoil';
-import filterStore from '../../stores/filterStore';
 import { v4 as uuidv4 } from 'uuid';
+import { useRecoilState } from 'recoil';
+import { filterColumns } from '../../stores/filterStore';
+import getColumnsUtil from '../../utils/getColumnsUtil';
 
-import authConfig from '../../auth_config.json';
+import './AddFilterButton.css';
 
 const AddFilterButton = ({ auth0 }: { auth0: Auth0ContextInterface }) => {
     const [dropdownState, setDropdownState] = useState(false);
-    const [filterState, setFilterState] = useRecoilState(filterStore);
-
-    const getSynopsis = async () => {
-        if (!auth0.isAuthenticated) return;
-
-        try {
-            if (filterState.synopsisData.columns.length) {
-                return;
-            }
-            const response = await (
-                await fetch(`${authConfig.apiBase}/synopsis`, {
-                    headers: {
-                        Authorization: `Bearer ${await auth0.getAccessTokenSilently()}`,
-                    },
-                })
-            ).json();
-
-            setFilterState({
-                ...filterState,
-                synopsisData: {
-                    columns: (
-                        response.data as Pick<
-                            FilterStore.State['synopsisData'],
-                            'columns'
-                        >
-                    ).columns,
-                },
-            });
-        } catch (error) {
-            throw new Error(`An error has occured:\n ${error}`);
-        }
-    };
+    const [columns, setColumns] = useRecoilState(filterColumns);
 
     return (
         <ButtonDropdown
@@ -52,23 +22,18 @@ const AddFilterButton = ({ auth0 }: { auth0: Auth0ContextInterface }) => {
             className='mb-3'
             isOpen={dropdownState}
             toggle={() => setDropdownState(!dropdownState)}
-            onClick={() => getSynopsis()}>
+            onClick={() =>
+                !columns.length && getColumnsUtil(auth0, setColumns)
+            }>
             <DropdownToggle>
                 <FontAwesomeIcon icon='plus' className='mr-2' /> Add Filter
             </DropdownToggle>
             <DropdownMenu style={{ margin: 0 }}>
-                {filterState.synopsisData.columns.map(
-                    ({ sample, sampleHeader }) => (
-                        <div key={uuidv4()}>
-                            <ColumnItem
-                                column={{
-                                    sampleHeader: sampleHeader,
-                                    sample: sample,
-                                }}
-                            />
-                        </div>
-                    )
-                )}
+                {columns.map((column) => (
+                    <div key={uuidv4()}>
+                        <ColumnList column={column} />
+                    </div>
+                ))}
             </DropdownMenu>
         </ButtonDropdown>
     );
---
+++ src/utils/getColumnsUtil.ts
@@ -0,0 +1,26 @@
+import { Auth0ContextInterface } from '@auth0/auth0-react';
+import { SetterOrUpdater } from 'recoil';
+import authConfig from '../auth_config.json';
+
+const getColumnsUtil = async (
+    auth0: Auth0ContextInterface,
+    setColumns: SetterOrUpdater<FilterStore.ColumnItem[]>
+) => {
+    if (!auth0.isAuthenticated) return;
+
+    try {
+        const response = await (
+            await fetch(`${authConfig.apiBase}/synopsis`, {
+                headers: {
+                    Authorization: `Bearer ${await auth0.getAccessTokenSilently()}`,
+                },
+            })
+        ).json();
+
+        setColumns(response.data.columns as Array<FilterStore.ColumnItem>);
+    } catch (error) {
+        throw new Error(`An error has occured:\n ${error}`);
+    }
+};
+
+export default getColumnsUtil;
